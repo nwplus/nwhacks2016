40e036da7ed17f1faba6bab59dc3a003833b56d4
Tristan Rice <rice@fn.lc>
2015-10-16 02:57:21 +0000

    Updated gitignore to include bower stuff

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"mime"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type User struct {
+	Email, Name, University, Location, TShirtSize, LinkedIn, GitHub, PersonalSite string
+	TravelReimbursement                                                           int
+	NeedsTravelReimbursement, Accepted, Waitlisted                                bool
+}
+
+func main() {
+
+	// Had to do this because returns svg as text/xml when running on AppEngine: http://goo.gl/hwZSp2
+	mime.AddExtensionType(".svg", "image/svg+xml")
+	db, err := gorm.Open("sqlite3", "/tmp/gorm.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.CreateTable(&User{})
+	s := &server{db}
+
+	r := mux.NewRouter()
+	sr := r.PathPrefix("/api").Subrouter()
+	sr.HandleFunc("/register", s.registerHandler)
+	r.HandleFunc("/{rest:.*}", s.staticHandler)
+	http.Handle("/", r)
+}
+
+type server struct {
+	db gorm.DB
+}
+
+func (s *server) staticHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("path:", r.URL.Path)
+	http.ServeFile(w, r, "static/"+r.URL.Path)
+}
+
+func (s *server) registerHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("path:", r.URL.Path)
+}
