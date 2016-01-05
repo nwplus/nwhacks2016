e0ebc7552b2b658d395a824a2cdb6a1ef169c447
Tristan Rice <rice@fn.lc>
2016-01-05 15:59:27 -0800

    Removed old registration code from go proxy

--- main.go
+++ main.go
@@ -9,16 +9,7 @@ import (
 	"net/url"
 
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
-
-	_ "github.com/mattn/go-sqlite3"
 )
-
-type User struct {
-	Email, Name, University, Location, TShirtSize, LinkedIn, GitHub, PersonalSite string
-	TravelReimbursement                                                           int
-	NeedsTravelReimbursement, Accepted, Waitlisted                                bool
-}
 
 var addr = flag.String("addr", ":8182", "the address to listen on")
 
@@ -27,40 +18,24 @@ func main() {
 
 	// Had to do this because returns svg as text/xml when running on AppEngine: http://goo.gl/hwZSp2
 	mime.AddExtensionType(".svg", "image/svg+xml")
-	db, err := gorm.Open("sqlite3", "/tmp/gorm.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	db.CreateTable(&User{})
-	s := &server{db}
 
 	r := mux.NewRouter()
-	//sr := r.PathPrefix("/api").Subrouter()
-	//sr.HandleFunc("/register", s.registerHandler)
 	apiServer, err := url.Parse("http://localhost:8183")
 	if err != nil {
 		log.Fatal(err)
 	}
 	r.Handle("/api/{rest:.*}", httputil.NewSingleHostReverseProxy(apiServer))
-	r.HandleFunc("/{rest:.*}", s.staticHandler)
+	r.HandleFunc("/{rest:.*}", staticHandler)
 	http.Handle("/", r)
 	log.Printf("Listening on %s", *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
-type server struct {
-	db gorm.DB
-}
-
-func (s *server) staticHandler(w http.ResponseWriter, r *http.Request) {
+func staticHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("path:", r.URL.Path)
 	if r.URL.Path == "/" {
 		http.ServeFile(w, r, "static/app.html")
 	} else {
 		http.ServeFile(w, r, "static/"+r.URL.Path)
 	}
-}
-
-func (s *server) registerHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("path:", r.URL.Path)
 }
